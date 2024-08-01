2c625357de4bf927d518bc9e818c25390b139f08
mervs <mervsen@gmail.com>
2024-08-01 12:55:49 +0300

    mark

--- src/app/features/ihale/ihale-karsilastir/ihale-karsilastir.component.ts
+++ src/app/features/ihale/ihale-karsilastir/ihale-karsilastir.component.ts
@@ -2,11 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TenderBid } from '../../../models/tender-bid';
 import { CompareBidsService } from '../../../services/compare-bids.service';
 import { TableModule } from 'primeng/table';
-import { JsonPipe, NgClass } from '@angular/common';
+import {  NgClass } from '@angular/common';
 import { Column } from '../../../models/column.interface';
 import { MultiSelectModule } from 'primeng/multiselect';
-import { FormsModule, NgModel, NgModelGroup } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { CompareColumn } from '../../../models/compare-column.interface';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 interface Price {
   title: string;
   bid: number;
@@ -27,7 +30,7 @@ interface CompareTableRow {
 @Component({
   selector: 'app-ihale-karsilastir',
   standalone: true,
-  imports: [TableModule, MultiSelectModule, FormsModule, NgClass],
+  imports: [TableModule, MultiSelectModule, FormsModule, NgClass, ToggleButtonModule, MenubarModule],
   templateUrl: './ihale-karsilastir.component.html',
   styleUrl: './ihale-karsilastir.component.scss'
 })
@@ -37,11 +40,13 @@ export class IhaleKarsilastirComponent implements OnInit {
   tableData! : any[];
   tableStyle = {width:"100%"}
   tenderData!: any[];
-  
+  tableMenuItems!: MenuItem[];
   data: any [] = [];
   selectedColumns! : CompareColumn[];
-  
+  colExpandValue = 2;
+
   bids!: TenderBid[];
+  markMin: boolean = false;
 
   constructor(
     private compareService: CompareBidsService
@@ -75,6 +80,16 @@ export class IhaleKarsilastirComponent implements OnInit {
             isUnit: column.isBirim,
             isTotal: column.isToplam,
             isAllTotal: column.isAllTotal,
+            color: 'bg-green-100'
+
+          });
+          this.compareColumns.push({
+            field: column.header + ' max',
+            header:'Maksimum' ,
+            isUnit: column.isBirim,
+            isTotal: column.isToplam,
+            isAllTotal: column.isAllTotal,
+            color: 'bg-red-100'
 
           });
 
@@ -91,10 +106,88 @@ export class IhaleKarsilastirComponent implements OnInit {
           
       }
       });
-      this.tableStyle.width = (this.compareColumns.length *7) +'rem';
+     
     this.selectedColumns = this.compareColumns;
-    
+    this.tableStyle.width = (this.selectedColumns.length *7) +'rem';
+    this.tableMenuItems = [
+      {
+          label: 'Sütun Ekle',
+          icon: 'pi pi-plus',
+          items: [
+            {
+              label: 'Minimum',
+              icon: 'pi pi-file-import',
+              //command: () => this.AddCol('Minimum'),
+            },
+            {
+              label: 'En Yüksek Değer',
+              icon: 'pi pi-file-export',
+              //command: () => this.exportAsExcel(),
+            },
+            {
+              label: 'Ortalama Değer',
+              icon: 'pi pi-calculator',
+              //command: () => (this.saveDraft()),
+            },
+            {
+              label: 'Bütçe',
+              icon: 'pi pi-calculator',
+              //command: () => (this.saveDraft()),
+            }
+          ]
+      },
+      {
+        label: 'İşaretle',
+          icon: 'pi pi-file-edit',
+          items: [
+            {
+              label: 'Minimum',
+              icon: 'pi pi-file-import',
+              command: () => {this.markMin=true; this.markMinFields(this.tableData,this.compareColumns)},
+          }
+        ]
+      }
+     ]
+
+     
   }
 
-  
+  markMinFields(rows:any[],columns: CompareColumn[]) {
+    if(this.markMin) {
+      for(const row of rows) {
+        for (const column of columns) {
+          if(column.isUnit || column.isTotal) {
+            //field name minus numbers + min
+            const minName = column.field.replace(/\d+$/, '') + ' min';
+            if(row[column.field]===row[minName]) {
+              row[column.field + 'mark'] = 'bg-green-100';
+            }
+          }
+        }
+      }
+      return;
+    }
+    else {
+    
+      this.unMarkAll(rows,columns);
+      return;
+    }
+ 
+  }
+  unMarkAll(rows:any[], columns:CompareColumn[]) {
+    for(const row of rows) {
+      for (const column of columns) {
+        if(row[column.field + 'mark']) {
+          row[column.field + 'mark']='';
+        }
+      }
+    }
+  }
+
+  AddCol(colType: string) {
+    
+   
+   console.log(this.compareColumns,this.compareColumns.filter(x=>x.header==='Minimum'))
+   this.tableStyle.width = (this.selectedColumns.length *7) +'rem';
+  }
 }
